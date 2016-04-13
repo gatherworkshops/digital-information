b4ea180101a1f7538b247b4a5c2a3d59285b38ef
Tanya Gray <Tanya@Wonkbook.local>
2016-04-14 11:26:44 +1200

    Updated schema to use autoincrement
    Signed-off-by: Tanya Gray <Tanya@Wonkbook.local>

--- _courses/web-data-with-sqlite/assets/projects/message-board/db/schema.sql
+++ _courses/web-data-with-sqlite/assets/projects/message-board/db/schema.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
-	user_id INTEGER PRIMARY KEY,
+	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	username TEXT(30),
 	password TEXT(64),
 	first_name TEXT(20),
@@ -13,7 +13,7 @@ CREATE TABLE users(
 
 DROP TABLE IF EXISTS messages;
 CREATE TABLE messages(
-	message_id INTEGER PRIMARY KEY,
+	message_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	content TEXT(300),
 	time_created TEXT(30),
 	user_id INTEGER FOREIGN_KEY
