42bb4824fd2cc666f3f6804cd065b2d009925551
Tanya Gray <Tanya@Wonkbook.local>
2016-04-13 20:22:55 +1200

    Site build
    Signed-off-by: Tanya Gray <Tanya@Wonkbook.local>

---
+++ _site/courses/web-data-with-sqlite/assets/projects/message-board/db/schema.sql
@@ -0,0 +1,22 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	user_id INTEGER PRIMARY KEY,
+	username TEXT(30),
+	password TEXT(64),
+	first_name TEXT(20),
+	last_name TEXT(20),
+	email TEXT(50),
+	photo BLOB(10000)
+);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages(
+	message_id INTEGER PRIMARY KEY,
+	content TEXT(300),
+	time_created TEXT(30),
+	user_id INTEGER FOREIGN_KEY
+);
+
+COMMIT;
